b1f3dfc699bf47765c699369b5eeaec1802d1ee1
willduty <willduty@yahoo.com>
2012-05-08 20:32:48 -0400

    registration mailer test

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,12 @@
+
 class UserMailer < ActionMailer::Base
-	default :from => 'wduty2012@gmail.com'
-	
-	def confirm_email(user)
-		@user = user
-		mail(:to => user.email, :subject => "Thanks for registering to mytunespage. You can login now")
-	
-	end
+  def confirm_email(user)
+    recipients "#{user.username} <#{user.email}>"
+    from       "My Tunespage"
+    subject    "Please activate your new account"
+    sent_on    Time.now
+    body       { :user => user, :url => 'Thanks for registering to mytunespage. You can login now.' }
+  end
 end
+
+
--- config/environment.rb
+++ config/environment.rb
@@ -5,5 +5,15 @@ require File.expand_path('../application', __FILE__)
 RorTunes::Application.initialize!
 
 
+config.action_mailer.delivery_method = :smtp
 
 
+ActionMailer::Base.smtp_settings = {
+  :address  => "smtp.gmail.com",
+  :port  => 25,
+  :user_name  => "wduty_2012@gmail.com",
+  :password  => "tester11",
+  :authentication  => :login
+}
+
+config.action_mailer.raise_delivery_errors = true
