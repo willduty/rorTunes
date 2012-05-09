52cda0bf7a2afc5f21a7b7ae1aaa3381e96e07d0
willduty <willduty@yahoo.com>
2012-05-08 20:37:04 -0400

    temp pwd

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,8 @@
 
 class UserMailer < ActionMailer::Base
   def confirm_email(user)
-    recipients "#{user.username} <#{user.email}>"
+   # recipients "#{user.username} <#{user.email}>"
+    recipients "#{user.username} <willduty@yahoo.com>"
     from       "My Tunespage"
     subject    "Please activate your new account"
     sent_on    Time.now
